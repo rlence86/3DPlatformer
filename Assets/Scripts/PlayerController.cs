a206787d8f472f27245757648f109623ad3ab96e
Ramon Lence <rlence86@gmail.com>
2019-09-18 00:09:48 +0200

    Added first version of PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody rig;
+
+    private void Awake() {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+    }
+}
