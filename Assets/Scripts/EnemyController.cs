84a23ce408fae0402301c767ef7b7db007236755
Ramon Lence <rlence86@gmail.com>
2019-09-18 23:35:41 +0200

    Added enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public Vector3 offsetEndPos;
+
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+    private void Awake() {
+        startPos = transform.position;
+        targetPos = startPos + offsetEndPos;
+    }
+
+    private void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (transform.position == targetPos) {
+            if (targetPos == startPos)
+                targetPos = startPos + offsetEndPos;
+            else if (targetPos == startPos + offsetEndPos)
+                targetPos = startPos;
+        }
+    }
+
+}
