1eb9ef3fafe133cb4428d06fe5f2a16a71f783aa
Ramon Lence <rlence86@gmail.com>
2019-09-18 23:04:14 +0200

    finished jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,11 +42,17 @@ public class PlayerController : MonoBehaviour
     }
 
     private void TryJump() {
-
-        Ray ray = new Ray(transform.position, Vector3.down);
-
-        if(Physics.Raycast(ray, 0.7f)) {
+        if( TouchingGround() ) {
             rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
+
+    private bool TouchingGround() {
+        Ray ray1 = new Ray(transform.position + new Vector3(0.5f, 0, 0.5f), Vector3.down);
+        Ray ray2 = new Ray(transform.position + new Vector3(0.5f, 0, -0.5f), Vector3.down);
+        Ray ray3 = new Ray(transform.position + new Vector3(-0.5f, 0, -0.5f), Vector3.down);
+        Ray ray4 = new Ray(transform.position + new Vector3(-0.5f, 0, 0.5f), Vector3.down);
+        
+        return Physics.Raycast(ray1, 0.7f) || Physics.Raycast(ray2, 0.7f) || Physics.Raycast(ray3, 0.7f) || Physics.Raycast(ray4, 0.7f);
     }
 }
